b0d037322c92a5fb108abd3eea8bf1f065910dc0
Aaron Bronow <abronow@gmail.com>
2026-05-27 11:58:52 -0700

    feat: add elegant CLI verification shortcuts for single and cross-file comparison

--- bin/yaml-chain.js
+++ bin/yaml-chain.js
@@ -1,4 +1,24 @@
 #!/usr/bin/env node
 import { createCli } from '../src/cli.js';
 
+const knownCommands = new Set(['init', 'append', 'verify', 'status', 'diff', 'show', 'help', '-h', '--help', '-V', '--version']);
+const args = process.argv.slice(2);
+const firstArg = args[0];
+
+if (firstArg && !firstArg.startsWith('-') && !knownCommands.has(firstArg)) {
+  const newArgs = ['verify'];
+  const secondArg = args[1];
+  
+  if (secondArg && !secondArg.startsWith('-')) {
+    // Shortcut for cross-file verification: yaml-chain file1.yaml file2.yaml -> verify file1.yaml -c file2.yaml
+    newArgs.push(firstArg, '-c', secondArg);
+    newArgs.push(...args.slice(2));
+  } else {
+    // Shortcut for standard verification: yaml-chain file1.yaml -> verify file1.yaml
+    newArgs.push(...args);
+  }
+  
+  process.argv = [...process.argv.slice(0, 2), ...newArgs];
+}
+
 createCli().parse(process.argv);
